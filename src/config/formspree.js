ca4ccc1f945710f76470841da383ad8178f9470c
Prachi Khutarkar <prachivk30@gmail.com>
2026-08-07 00:31:27 +0530

    Delete src directory

--- src/config/formspree.js
+++
@@ -1,2 +0,0 @@
-// Formspree action URL placeholder. If you want the contact form live, replace the URL in src/components/Contact.jsx form action or create this file and import it.
-export const FORMSPREE_ENDPOINT = 'https://formspree.io/f/YOUR_FORMSPREE_ID'
